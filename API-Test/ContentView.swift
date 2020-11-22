44cb87bc159c26097a4f7f5a26678881e714fa11
Roman Vostrikov <vostrikov.berlin@gmail.com>
2020-11-22 18:44:05 +0100

    Initial Commit

---
+++ API-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  API-Test
+//
+//  Created by Roman Vostrikov on 11/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
